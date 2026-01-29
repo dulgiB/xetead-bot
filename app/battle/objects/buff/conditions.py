3e605cf33cecc84b3d7cc3dec930278444db3e5f
dulgiB <cony717@naver.com>
2026-01-29 20:35:38 +0900

    condition 도입

---
+++ app/battle/objects/buff/conditions.py
@@ -0,0 +1,53 @@
+import abc
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Optional
+
+from battle.core.commands.models import CharacterCommandData
+from battle.objects.models import CharacterId
+
+if TYPE_CHECKING:
+    from battle.core.battlefield_context import BattlefieldContext
+
+
+@dataclass(frozen=True)
+class Condition(abc.ABC):
+    value: Optional[int] = None
+
+    @abc.abstractmethod
+    def is_applied(
+        self,
+        context: "BattlefieldContext",
+        holder: CharacterId,
+        attacker_or_target: Optional[CharacterId],
+    ) -> bool:
+        pass
+
+
+@dataclass(frozen=True)
+class IsInSameColumnCondition(Condition):
+    def is_applied(
+        self,
+        context: "BattlefieldContext",
+        holder: CharacterId,
+        attacker_or_target: Optional[CharacterId],
+    ) -> bool:
+        return context.find_character_position(
+            holder
+        ) == context.find_character_position(attacker_or_target)
+
+
+@dataclass(frozen=True)
+class WasNotAttackedCondition(Condition):
+    def is_applied(
+        self,
+        context: "BattlefieldContext",
+        holder: CharacterId,
+        attacker_or_target: Optional[CharacterId],
+    ) -> bool:
+        for result in context.prev_round_results:
+            if isinstance(result.command_data, CharacterCommandData) and holder in [
+                data.target_id for data in result.command_data.damage_list
+            ]:
+                return False
+
+        return True
