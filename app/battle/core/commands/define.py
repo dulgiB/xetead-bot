03933538b27bdfbb23d8a4662af1be0973c6771d
dulgiB <cony717@naver.com>
2026-01-29 20:26:04 +0900

    RoundPhaseType을 string enum으로 수정

--- app/battle/core/commands/define.py
+++ app/battle/core/commands/define.py
@@ -1,15 +1,15 @@
 from enum import Enum
 
 
-class RoundPhaseType(Enum):
+class RoundPhaseType(str, Enum):
     # 적군 행동 선언, 이동/버프 선적용
-    ENEMY_PRE_ACTION = 1
+    ENEMY_PRE_ACTION = "적 행동 선언"
 
     # 아군 행동
-    ALLY_ACTION = 2
+    ALLY_ACTION = "아군 행동"
 
     # 적군 공격 정산
-    ENEMY_POST_ACTION = 3
+    ENEMY_POST_ACTION = "적 공격 정산"
 
     # 버프/디버프 턴수 차감 및 제거, 다음 라운드 시작 대기
-    BUFF_UPDATE_AND_NEXT_ROUND_STANDBY = 4
+    BUFF_UPDATE_AND_NEXT_ROUND_STANDBY = "라운드 종료"
