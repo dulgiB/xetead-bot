334e9a4d5a60b30dbfa1d9901ab4dbcba26da3ad
dulgiB <cony717@naver.com>
2026-01-29 20:23:11 +0900

    CommandCalculator 추가

--- app/battle/core/commands/models.py
+++ app/battle/core/commands/models.py
@@ -3,13 +3,18 @@ from dataclasses import KW_ONLY, dataclass, field
 from typing import TYPE_CHECKING, Optional
 
 from battle.objects.buff.buff_base import BuffAddEvent, BuffRemoveEvent
-from battle.objects.buff.buff_events import BuffEvent
-from battle.objects.character.buffed_stats import BuffedStats
-from battle.objects.models import BaseValueIndicator, CharacterId
 from battle.objects.define import ActionType, BattlefieldColumnIndex
+from battle.objects.models import (
+    BaseValueIndicator,
+    CharacterId,
+    FloatValueModifier,
+    IntValueModifier,
+)
 
 if TYPE_CHECKING:
     from battle.core.battlefield_context import BattlefieldContext
+    from battle.objects.buff.buff_events import BuffEvent
+    from battle.objects.character.buffed_stats import BuffedStats
 
 
 # user input -> parse() -> list[CommandBase] -> expand_xxx_command() ->
@@ -79,16 +84,37 @@ class CharacterCommandData(CommandData):
 @dataclass(frozen=True)
 class BanResult:
     is_banned: bool
-    source: BuffEvent
+    source: "BuffEvent"
 
 
-# TODO: something intermediate that each BuffEvent can mutate
-class CharacterCommandCalculator:
-    def __init__(
-        self, command_data: CharacterCommandData, context: "BattlefieldContext"
-    ):
+@dataclass
+class DamageCalculateData:
+    base: DamageData
+    modifiers: list[IntValueModifier | FloatValueModifier]
+
+
+@dataclass
+class HealCalculateData:
+    base: HealData
+    modifiers: list[IntValueModifier | FloatValueModifier]
+
+
+class CommandCalculator:
+    def __init__(self, command_data: CommandData, context: "BattlefieldContext"):
         self.command_data = command_data
         self.context = context
+        self.buffed_stats_by_character: dict[CharacterId, BuffedStats] = {
+            char_id: BuffedStats(character.status, {})
+            for char_id, character in context.characters.items()
+        }
+
+        self.ban_event_list: list[BuffEvent] = []
+        self.damage_data_list: list[DamageCalculateData] = [
+            DamageCalculateData(data, []) for data in command_data.damage_list
+        ]
+        self.heal_data_list: list[HealCalculateData] = [
+            HealCalculateData(data, []) for data in command_data.heal_list
+        ]
 
 
 @dataclass(frozen=True)
