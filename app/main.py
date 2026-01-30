2c547bddb986a28696b7aa445312d1b8f7394bb3
dulgiB <cony717@naver.com>
2026-01-30 12:53:01 +0900

    TODO: main 구현

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from battle.core.battlefield_context import BattlefieldContext
+
+# gc = gspread.service_account(".config/service_account.json")
+
+field = BattlefieldContext()
+
+# sh = gc.open_by_key("1QEvg9gjvBY4iuL6Dvol3wr8UQTVtfXiJ6Nk3i5glL7M")
+# worksheet = sh.worksheet("버프")
+
+# print(worksheet.get_notes())
